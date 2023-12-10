ec118ee3689b0008eaa51abc071fe4fdf1f71513
viktordenac <viktor.denac@gmail.com>
2023-12-10 23:12:17 +0100

    Naloga 2 - Fixed HillClimbing.kt

--- src/main/kotlin/HillClimbing.kt
+++ src/main/kotlin/HillClimbing.kt
@@ -1,63 +1,63 @@
+import kotlin.math.pow
+
 class HillClimbing(var stepSize : Double = 0.5, maxFes: Int) : Algorithm(maxFes) {
 
-    override fun execute(problem: Problem): Solution {
-        // Initialization
-        var solution = problem.randomSolution()
-        var currentFes = 0
-        solution = problem.fitness(solution)
+    fun neighbors(solution: Solution, stepSize: Double, problem: Problem): MutableList<Solution> {
+        var neighbors = mutableListOf<Solution>()
+        val dimensions = solution.candidate.size
 
-        var bestSolution = solution
-
-        val prevPoints = MutableList(0) { Solution() }
-        prevPoints.add(solution)
-
-        var neighbors = problem.neighbors(solution, stepSize)
-        var repetitions = 0
-
-        // Main Loop
-        while (currentFes <= maxFes-1){
-            currentFes += 4
-
-            // Replenish neighbors if the list is empty
-            if (neighbors.isEmpty()) {
-                neighbors = problem.neighbors(bestSolution, stepSize)
+        for (i in 0..<3.0.pow(dimensions.toDouble()).toInt()) {
+            val neighbor = Solution()
+            var ternary = i.toString(3).padStart(dimensions, '0')
+            for (j in 0..<dimensions) {
+                val dimensionIndex = ternary[j].toString().toInt() - 1
+                neighbor.candidate.add(solution.candidate[j] + dimensionIndex * stepSize)
             }
+            if (neighbor.candidate == solution.candidate) continue
+            neighbors.add(problem.setFeasible(neighbor))
+        }
+        neighbors = neighbors.distinct().toMutableList()
+        return neighbors
+    }
 
-            // Remove already visited neighbors
+    override fun execute(problem: Problem): Solution {
+        var solution = problem.randomSolution()
+        solution = problem.fitness(solution)
+        var bestSolution = solution
+        var prevPoints = MutableList(0) { Solution() }
+        prevPoints.add(solution)
+        var neighbors = neighbors(solution, stepSize, problem)
+        var repetitions = 0
+        repeat(maxFes - 1) {
+            if (neighbors.isEmpty()) {
+                neighbors = neighbors(bestSolution, stepSize, problem)
+            }
             while (neighbors.isNotEmpty() && prevPoints.contains(neighbors[0])) {
                 neighbors.removeAt(0)
             }
 
-            // If neighbors is still empty, add a random solution
             if (neighbors.isEmpty()) {
                 neighbors.add(problem.randomSolution())
             }
 
-            // Select a neighbor
             var neighbor = neighbors[0]
             neighbors.removeAt(0)
             neighbor = problem.fitness(neighbor)
 
-            // Fitness Comparison and Update
             if (neighbor.fitnessValue == bestSolution.fitnessValue) {
                 repetitions += 1
             } else if (neighbor.fitnessValue < bestSolution.fitnessValue) {
                 bestSolution = neighbor
                 repetitions = 0
             }
-
-            // Add the neighbor to the list of visited points
             prevPoints.add(neighbor)
-//TODO fix the logic of this and above the neigbor.fitness if statement
-            // Repetition Check
+
             if (repetitions > 5) {
                 repetitions = 0
                 neighbors.clear()
                 neighbors.add(problem.randomSolution())
             }
         }
-
-        // Return the best solution found
         return bestSolution
     }
 }
