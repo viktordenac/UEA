c88d45cbdf0e60c329f23799d9a6a5f246077d1d
viktordenac <viktor.denac@gmail.com>
2023-12-11 11:54:40 +0100

    Naloga 3 - PSO complete

--- src/main/kotlin/algorithms/PSO.kt
+++ src/main/kotlin/algorithms/PSO.kt
@@ -2,7 +2,7 @@ package algorithms
 import Algorithm
 import Problem
 import solution.ParticleSolution
-import solution.Solution
+import kotlin.random.Random
 
 class PSO(maxFes: Int) : Algorithm(maxFes) {
     var particles = mutableListOf<ParticleSolution>()
@@ -17,7 +17,47 @@ class PSO(maxFes: Int) : Algorithm(maxFes) {
         }
     }
 
-    override fun execute(problem: Problem): Solution {
-        TODO("Not yet implemented")
+    override fun execute(problem: Problem): ParticleSolution {
+        var g = ParticleSolution()
+
+        repeat(sizeOfPopulation) { i ->
+            particles[i].candidate = problem.randomSolution().candidate
+            particles[i].bestCandidate = particles[i].returnCandidate()
+            particles[i].velocity =
+                MutableList(problem.dimensions) { Random.nextDouble(problem.lowerBound[it], problem.upperBound[it]) }
+
+            particles[i].fitnessValue = problem.fitness(particles[i]).fitnessValue
+            if (particles[i].fitnessValue < g.bestFitnessValue) {
+                g = particles[i].copy()
+            }
+        }
+
+        repeat(maxFes - (sizeOfPopulation)) {
+            repeat(sizeOfPopulation ) { i ->
+                repeat(problem.dimensions ) {
+                    var r1 = Random.nextDouble()
+                    var r2 = Random.nextDouble()
+                    particles[i].velocity[it] =
+                        w * particles[i].velocity[it] + c1 * r1 * (particles[i].bestCandidate[it] - particles[i].candidate[it]) + c2 * r2 * (g.bestCandidate[it] - particles[i].candidate[it])
+                }
+                repeat(problem.dimensions - 1) {
+                    particles[i].candidate[it] =
+                        particles[i].returnCandidateAtI(it) + particles[i].returnVelocityAtI(it)
+                }
+                particles[i].candidate = problem.setFeasible(particles[i]).candidate
+
+                particles[i].fitnessValue = problem.fitness(particles[i]).fitnessValue
+                if (particles[i].fitnessValue < particles[i].fitnessValue) {
+                    particles[i].bestCandidate = particles[i].returnCandidate()
+                }
+
+                if (particles[i].fitnessValue < g.bestFitnessValue) {
+                    g.bestFitnessValue = particles[i].returnFitnes()
+                    g.bestCandidate = particles[i].returnCandidate()
+                }
+            }
+        }
+
+        return g
     }
 }
